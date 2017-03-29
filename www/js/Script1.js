e04a770d38fa0774fc15d5347acde076813de2ab
Jakub koza <Jakub koza>
2017-03-29 23:57:30 +0200

    Poczatek budowanie wygladu aplikacji

--- www/js/Script1.js
+++ www/js/Script1.js
@@ -1,1 +1,24 @@
-// JavaScript source code
+window.addEventListener("DOMContentLoaded", start);
+var myObj;
+function start() {
+    pobierzJSON();
+}
+function pobierzJSON() {
+
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          myObj = JSON.parse(this.responseText);
+
+
+        }
+    };
+    xmlhttp.open("GET", "http://k.lk.kei.pl/kuba.txt", true);
+    xmlhttp.send();
+
+
+
+
+
+}
