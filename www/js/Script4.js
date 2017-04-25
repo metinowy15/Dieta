95bd7782666a355164f256374bd51aa59739dbf0
Jakub koza <Jakub koza>
2017-04-26 00:47:53 +0200

    poprawki

--- www/js/Script4.js
+++ www/js/Script4.js
@@ -3,6 +3,7 @@ var zaznaczone = [];
 var ListaPosilkow = [];
 var pojemnik;
 var listyzLokalki;
+let TwojeListy;
 var nowaLista = {
 
     nazwaListy:"",
@@ -17,11 +18,18 @@ var nowaLista = {
 }
 
 function start4() {
+
+
+
+    
+    TwojeListy = document.getElementById("TwojeListy");
     ZapiszListe = document.getElementById("ZapiszListe");
     UsunZListy = document.getElementById("UsunZListy");
     ZapiszListe.addEventListener("click", zapiszListe);
+    UsunZListy.addEventListener("click", usunZListy);
     pojemnik = document.getElementById("NowePosilki");
     listyzLokalki = localStorage.getItem("listObj");
+    if (listyzLokalki !== undefined) { ladujListe();}
    $(document).on("pageshow", "#ListaPosilkow", reloadListaPosilkow);
 
 
@@ -33,75 +41,34 @@ function reloadListaPosilkow() {
 
 
 
-
-
     stringOnSession = sessionStorage.getItem("eatObj");
     if (stringOnSession == undefined) { return; }
     if (stringOnSession == null) { return;}
     ListaPosilkow = JSON.parse(stringOnSession).tablica;
-    if (ListaPosilkow.length > 0) {
-        pojemnik.empty();
-        for (let i = 0; i < ListaPosilkow.length; i++) {
-            nowy = document.createElement("div");
-            nowy.classList.add("posilek");
-            nowy.innerHTML = "<p><b>" + ListaPosilkow[i].nazwa +"</b>, godz:" + ListaPosilkow[i].czas + ", waga:" + ListaPosilkow[i].waga + "g, kal:" + ListaPosilkow[i].dziennaDawka + "% dziennej dawki</p>";
-            for (let j = 0; j < ListaPosilkow[i].produktyTab.length; j++) {
-
-                nowy.innerHTML += "<li><b>" + ListaPosilkow[i].produktyTab[j].nazwa + "</b>, bialka:" + ListaPosilkow[i].produktyTab[j].bialka + ", tluszcze:" + ListaPosilkow[i].produktyTab[j].tluszcze + ", weglowodane:" + ListaPosilkow[i].produktyTab[j].weglowodane + "</li>";
-
-            }
-            nowy.innerHTML += "<p>w sumie=Bialek:<b>" + sumujBialka(ListaPosilkow[i].produktyTab) + "g</b>, Tluszczy:<b>" + sumujTluszcze(ListaPosilkow[i].produktyTab) + "g</b>, Weglowodanych:<b>" + sumujWegle(ListaPosilkow[i].produktyTab)+"g</b></p>";
-            pojemnik.appendChild(nowy);
-            nowy.addEventListener("click", callbackDla(nowy, ListaPosilkow[i]));
-           
-        }
-
-
+    if (ListaPosilkow == undefined) { return;}
+    if (ListaPosilkow.length > 0) { rysujListe(); }
+      
 
     }
     
 
 
-    function callbackDla(nowyElement,elementTablicy) {
-
-        return function (data) {
-
-            for (let k = 0; k < zaznaczone.length; k++) {
-
-                if (zaznaczone[k].nazwa ===elementTablicy.nazwa) {
-                    zaznaczone.splice(k, 1);
-
-                    nowyElement.classList.remove("posilekZaznaczony");
-                    return;
-
-                }
-
-            }
-            zaznaczone[zaznaczone.length] = elementTablicy;
-
-            nowyElement.classList.add("posilekZaznaczony");
+  
 
 
 
-        
-        }
-
-    }
-
-
-}
 function zapiszListe() {
     
 
 function onPrompt(result) {
 
     if (result.buttonIndex == 2) {
-        return;
+       return;
     } else {
         let procenty=sumujProcenty(zaznaczone);
         nowaLista = {
 
-            nazwaListy: result.input1,
+            nazwaListy:result.input1,
             posilki: zaznaczone,
             procenty: procenty,
             aktywna:false
@@ -141,7 +108,7 @@ function onPrompt(result) {
 
 
     }
-    
+  
 
     }
 
@@ -159,22 +126,164 @@ function onPrompt(result) {
 
 }
 function odNowa() {
+    ladujListe();
+    zaznaczone = [];
+
+    tabElement = document.getElementsByClassName("posilekZaznaczony");
+    for (var i = 0; i < tabElement.length; i++) {
+
+        pojemnik.removeChild(tabElement[i]); 
+
+    }
+   
+    
+
+
+}
+function usunZListy() {
+
+
+
+
+
+
+    zaznaczone = [];
+
+    for (let i = 0; i < ListaPosilkow.length; i++) {
+
+        for (let j = 0; j < zaznaczone.length; j++) {
+
+            if (zaznaczone[j].nazwa == ListaPosilkow[z].nazwa) {
+
+                ListaPosilkow.splice(z, 1);
+            }
+
+        }
+
+    }
+    sessionStorage.setItem("eatObj", JSON.stringify(ListaPosilkow));
+
+
+
+
+    tabElement = document.getElementsByClassName("posilekZaznaczony");
+    for (var i = 0; i < tabElement.length; i++) {
+
+        pojemnik.removeChild(tabElement[i]);
+
+    }
+
+
+}
+function ladujListe() {
+  
+    listyzLokalki = localStorage.getItem("listObj");
+    var tmp = JSON.parse(listyzLokalki);
+    if (tmp == undefined) {
+        return;
+    }
+    if (tmp == null) { return;}
+
+    for (let i = 0; i < tmp.tablica.length; i++) {
+
+        let nowy = document.createElement("option");
+        nowy.value = tmp.tablica[i].nazwaListy;
+        nowy.innerHTML = tmp.tablica[i].nazwaListy + "  " + tmp.tablica[i].procenty + "% CPM" + "lista jest ";
+        nowy.innerHTML +=(tmp.tablica[i].aktywna == true) ? "aktywna" : "nie aktywna";
+        
+        TwojeListy.appendChild(nowy);
+
+
+    }
+
+
+}
+function wybierzElement(aktywny) {
 
     listyzLokalki = localStorage.getItem("listObj");
     var tmp = JSON.parse(listyzLokalki);
-    let nazwa = tmp.tablica[0].nazwaListy;
-    let TwojeListy = document.getElementById("TwojeListy");
-    TwojeListy.innerHTML = " <option>"+nazwa+"</option>"
+    if (tmp == undefined) {
+        return;
+    }
+    if (aktywny == null) {
 
-    zaznaczone = [];
-    
-    while (pojemnik.firstChild) {
-        if (pojemnik.firstChild.classList.contains("posilekZaznaczony")) {
-            pojemnik.removeChild(pojemnik.firstChild);
+        for (let i = 0; i < tmp.tablica.length; i++) {
+            if (tmp.tablica[i].nazwaListy == TwojeListy.value) {
+
+                ListaPosilkow = tmp.tablica[i].posilki;
+                rysujListe()
+
+            }
+
         }
 
+
+
+    } else {
+
+
+
+        for (let i = 0; i < tmp.tablica.length; i++) {
+            if (tmp.tablica[i].aktywna == true) {
+
+                ListaPosilkow = tmp.tablica[i].posilki;
+                 rysujListe()
+
+
+            }
+
         }
-    
+
+
+    }
+
+
+}
+function rysujListe(){
+    var nowy;
+    pojemnik.empty();
+    for (let i = 0; i < ListaPosilkow.length; i++) {
+        nowy = document.createElement("div");
+        nowy.classList.add("posilek");
+        nowy.innerHTML = "<p><b>" + ListaPosilkow[i].nazwa +"</b>, godz:" + ListaPosilkow[i].czas + ", waga:" + ListaPosilkow[i].waga + "g, kal:" + ListaPosilkow[i].dziennaDawka + "% dziennej dawki</p>";
+        for (let j = 0; j < ListaPosilkow[i].produktyTab.length; j++) {
+
+            nowy.innerHTML += "<li><b>" + ListaPosilkow[i].produktyTab[j].nazwa + "</b>, bialka:" + ListaPosilkow[i].produktyTab[j].bialka + ", tluszcze:" + ListaPosilkow[i].produktyTab[j].tluszcze + ", weglowodane:" + ListaPosilkow[i].produktyTab[j].weglowodane + "</li>";
+
+        }
+        nowy.innerHTML += "<p>w sumie=Bialek:<b>" + sumujBialka(ListaPosilkow[i].produktyTab) + "g</b>, Tluszczy:<b>" + sumujTluszcze(ListaPosilkow[i].produktyTab) + "g</b>, Weglowodanych:<b>" + sumujWegle(ListaPosilkow[i].produktyTab)+"g</b></p>";
+        pojemnik.appendChild(nowy);
+        nowy.addEventListener("click", callbackDla(nowy, ListaPosilkow[i]));
+           
+    }
+
+
+
+    function callbackDla(nowyElement, elementTablicy) {
+
+        return function (data) {
+
+            for (let k = 0; k < zaznaczone.length; k++) {
+
+                if (zaznaczone[k].nazwa === elementTablicy.nazwa) {
+                    zaznaczone.splice(k, 1);
+
+                    nowyElement.classList.remove("posilekZaznaczony");
+                    return;
+
+                }
+
+            }
+            zaznaczone[zaznaczone.length] = elementTablicy;
+
+            nowyElement.classList.add("posilekZaznaczony");
+
+
+
+
+        }
+
+    }
 
 
 }
